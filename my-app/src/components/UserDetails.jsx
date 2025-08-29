a38cb2e4164dfea2a73872204f692591cac1a7b9
code-shub09 <singhshubham6135@gmail.com>
2025-08-29 05:36:45 +0530

    css-upd

--- my-app/src/components/UserDetails.jsx
+++ my-app/src/components/UserDetails.jsx
@@ -1,26 +1,39 @@
 import React, { useState } from "react";
 import { BiUserCircle } from "react-icons/bi"; // avatar icon
-
+import boy from "../assets/boy.png"
 function ReviewDetails() {
   const [name, setName] = useState("Shubham Singh");
   const phone = "+919873314150";
 
+
   const maxLength = 30;
 
   return (
+    <>
+    <hr />
     <div className="Review">
         <h3>Review Your Details</h3>
         <div className="Review-box"> 
-            <div className="Review-img"></div>
-            <div className="Review-name"></div>
+            <div className="Review-img"><div className="Review-img2"><img src={boy}  alt="user" /></div></div>
+            <div className="Review-name">
+                <h6>Name</h6>
+                <div className="div-num">
+
+                    Shubham singh
+
+
+                </div>
+            </div>
         </div>
         <div className="Review-Num">
             <p>Your Phone Number</p>
             <p>8178568199</p>
         </div>
-        
+
      
     </div>
+    </>
+    
   );
 }
 
